6b2491b0f8356b5d019fbcd0ad6da3eb88892085
Adam G <adamwizzy@hotmail.co.uk>
2020-11-16 17:18:25 +0000

    Updated program to pull data from sql database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,8 +3,6 @@ require 'bookmark'
 describe Bookmark do
 
   it "Returns an array when .all class method called" do
-    expect(Bookmark.all).to eq(["http://www.myspace.com",
-    "http://www.onemorelevel.com",
-    "http://www.google.com"])
+    expect(Bookmark.all).to be_instance_of(Array)
   end
 end
